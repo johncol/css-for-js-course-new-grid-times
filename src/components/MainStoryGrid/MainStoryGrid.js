d9b1c96e623b24afb87833e08bf5a29d30c373b6
John <john.19col@gmail.com>
2024-01-31 06:18:09 -0800

    Update MainStoryGrid grid areas configuration for larger viewports

--- src/components/MainStoryGrid/MainStoryGrid.js
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -12,10 +12,11 @@ import MainStory from '../MainStory';
 import SecondaryStory from '../SecondaryStory';
 import OpinionStory from '../OpinionStory';
 import Advertisement from '../Advertisement';
+import { QUERIES } from '../../constants';
 
 const MainStoryGrid = () => {
   return (
-    <Wrapper>
+    <Grid>
       <MainStorySection>
         <MainStory {...MAIN_STORY} />
       </MainStorySection>
@@ -40,11 +41,12 @@ const MainStoryGrid = () => {
       <AdvertisementSection>
         <Advertisement />
       </AdvertisementSection>
-    </Wrapper>
+    </Grid>
   );
 };
 
-const Wrapper = styled.div`
+const Grid = styled.div`
+  margin-bottom: 48px;
   display: grid;
   grid-template-areas:
     'main-story'
@@ -52,7 +54,20 @@ const Wrapper = styled.div`
     'opinion-stories'
     'advertisement';
   gap: 48px;
-  margin-bottom: 48px;
+
+  @media ${QUERIES.tabletOnly} {
+    grid-template-areas:
+      'main-story      secondary-stories'
+      'advertisement   advertisement'
+      'opinion-stories opinion-stories';
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-areas:
+      'main-story   secondary-stories opinion-stories'
+      'main-story   advertisement     advertisement';
+  }
+
 `;
 
 const MainStorySection = styled.section`
