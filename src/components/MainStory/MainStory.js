b84837597a49022bf0c8d3ae45eec9d2bef7f1a1
John <john.19col@gmail.com>
2024-01-31 19:17:31 -0800

    Remove ellipsis from "continue reading"

--- src/components/MainStory/MainStory.js
+++ src/components/MainStory/MainStory.js
@@ -19,7 +19,7 @@ const MainStory = ({
       <Abstract>
         <Location>{location}</Location> — {abstract}
       </Abstract>
-      <ReadMore href="/story">Continue Reading…</ReadMore>
+      <ReadMore href="/story">Continue Reading</ReadMore>
     </Wrapper>
   );
 };
